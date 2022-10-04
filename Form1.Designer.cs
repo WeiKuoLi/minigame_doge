1accb4ce61d3439a07d040ec8ce8e1e18303e78f
VIGO <VIGO@DESKTOP-THV65I7>
2022-10-05 00:06:44 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.doge = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.doge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(193, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 137);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click me!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox2
+            // 
+            this.doge.Image = global::WinFormsApp1.Properties.Resources.doge;
+            this.doge.Location = new System.Drawing.Point(507, 153);
+            this.doge.Name = "pictureBox2";
+            this.doge.Size = new System.Drawing.Size(221, 220);
+            this.doge.TabIndex = 2;
+            this.doge.TabStop = false;
+            this.doge.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(857, 862);
+            this.Controls.Add(this.doge);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.doge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+         
+        private PictureBox doge;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
